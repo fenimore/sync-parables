2f0f3de23d8251959e1d0f5f9b0a60af5c0f1ca8
polypmer <exorable.ludos@gmail.com>
2017-03-11 10:58:49 -0500

    Add lock for barber state

--- sleepingbarber.go
+++ sleepingbarber.go
@@ -13,23 +13,14 @@ const (
 	cutting
 )
 
-// Customer
-const (
-	going = iota
-	_     // checking
-	_     // cutting
-	waiting
-)
-
 type Barber struct {
-	name    string
-	state   int
-	wokenUp chan struct{}
+	sync.Mutex
+	name  string
+	state int
 }
 
 type Customer struct {
-	name  string
-	state int
+	name string
 }
 
 func NewBarber() (b *Barber) {
@@ -41,24 +32,27 @@ func NewBarber() (b *Barber) {
 
 func NewCustomer() (c *Customer) {
 	return &Customer{
-		name:  "George",
-		state: going,
+		name: "George",
 	}
 }
 
 // Barber thread
 func barber(b *Barber, wr chan *Customer, wakers chan *Customer) {
 	for {
+		b.Lock()
 		b.state = checking
+		b.Unlock()
 		// checking the waiting room
-		fmt.Printf("Checking, %d, for customer room: %d\n", b.state, len(wr))
+		fmt.Printf("Checking, %s, for customer room: %d\n", stateLog[b.state], len(wr))
 		time.Sleep(time.Millisecond * 10)
 		select {
 		case c := <-wr:
 			HairCut(c, b)
 		default:
 			fmt.Printf("Sleeping Barber\n")
+			b.Lock()
 			b.state = sleeping
+			b.Unlock()
 			c := <-wakers
 			fmt.Printf("Woken by %p\n", c)
 			HairCut(c, b)
@@ -66,49 +60,72 @@ func barber(b *Barber, wr chan *Customer, wakers chan *Customer) {
 	}
 }
 
+func HairCut(c *Customer, b *Barber) {
+	b.Lock()
+	b.state = cutting
+	b.Unlock()
+	// cut some hair
+	fmt.Printf("Cutting  %p's hair\n", c)
+	time.Sleep(time.Millisecond * 100)
+	wg.Done()
+}
+
 // customer goroutine
 // just fizzles out if it's full, otherwise the customer
 // is passed along to the channel handling it's haircut etc
 func customer(c *Customer, b *Barber, wr chan<- *Customer, wakers chan<- *Customer) {
 	// arrive
-	time.Sleep(time.Millisecond * 150)
+	fmt.Printf("Customer %p comes in to: %s barber, room: %d, wake: %d\n",
+		c, stateLog[b.state], len(wr), len(wakers))
+	time.Sleep(time.Millisecond * 50)
 	// Check on barber
-	defer fmt.Printf("Customer %p comes in to: %d, room: %d\n", c, b.state, len(wr))
+	b.Lock()
+	defer b.Unlock()
 	switch b.state {
 	case sleeping:
+		fmt.Printf("Sleeping barber %p, room: %d, wake: %d\n", c, len(wr), len(wakers))
 		select {
 		case wakers <- c:
 		default:
-			wr <- c // wakers is full
+			fmt.Printf("Someone else is waking the barber, %p goes to waiting room\n", c)
+			select {
+			case wr <- c:
+			default:
+				wg.Done()
+			}
 		}
-		return
-	case cutting, checking:
+	// TODO: fallthrough?
+	case cutting:
 		select {
 		case wr <- c:
 		default:
 			// full, leave shop
 			wg.Done()
 		}
+	case checking:
+		select {
+		case wr <- c:
+		default:
+			// full, leave shop
+			wg.Done()
+		}
+
 	}
 }
 
-func HairCut(c *Customer, b *Barber) {
-	b.state = cutting
-	c.state = cutting
-	// cut some hair
-	fmt.Printf("Cutting  %p's hair\n", c)
-	time.Sleep(time.Millisecond * 50)
-	wg.Done()
-}
-
 var wg *sync.WaitGroup // Amount of potentional customers
+var stateLog = map[int]string{
+	0: "sleeping",
+	1: "checking",
+	2: " cutting",
+}
 
 func main() {
 	//lock = new(sync.Mutex)
 	b := NewBarber()
 	b.name = "Sam"
-	WaitingRoom := make(chan *Customer, 5) // 5 chairs
-	Wakers := make(chan *Customer, 1)      // only one waker at a time
+	WaitingRoom := make(chan *Customer, 15) // 5 chairs
+	Wakers := make(chan *Customer, 1)       // only one waker at a time
 	go func() {
 		barber(b, WaitingRoom, Wakers)
 	}()
@@ -117,7 +134,7 @@ func main() {
 	wg.Add(10)
 	// Spawn customers
 	for i := 0; i < n; i++ {
-		time.Sleep(time.Millisecond * 20)
+		time.Sleep(time.Millisecond * 50)
 		c := NewCustomer()
 		go customer(c, b, WaitingRoom, Wakers)
 	}
